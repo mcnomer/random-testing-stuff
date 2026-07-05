d074f4337c91355fc7510305f78a3e253d44042c
Omer <57284289+mcnomer@users.noreply.github.com>
2026-07-05 16:12:51 +0100

    made more tabs for view transitions demo

--- web-stuff/view_transitions_demo/app.js
+++ web-stuff/view_transitions_demo/app.js
@@ -1,31 +1,65 @@
 const toggleButton = document.querySelector("button.corner-toggle");
+const upNextButton = document.querySelector(".tab-name.tab-up-next");
 const lyricsButton = document.querySelector(".tab-name.tab-lyrics");
+const creditsButton = document.querySelector(".tab-name.tab-credits");
 const playerContainer = document.querySelector("main.player");
+const tabContentContainer = document.querySelector(".tab-content");
 
-let lyricsOpen = true;
+const tabs = ["up-next", "lyrics", "credits"];
+let tabOpen = "up-next";
 
-function toggleLyrics() {
-    if (lyricsOpen) {
-        playerContainer.className = "player";
-    } else {
-        playerContainer.className = "player player-tabopen player-lyricsopen";
+function switchTab(newTab = "") {
+    tabOpen = newTab;
+    switch (tabOpen) {
+        case "up-next":
+            playerContainer.className =
+                "player player-tabopen player-upnextopen";
+            break;
+        case "lyrics":
+            playerContainer.className =
+                "player player-tabopen player-lyricsopen";
+            break;
+        case "credits":
+            playerContainer.className =
+                "player player-tabopen player-creditsopen";
+            break;
+
+        default:
+            playerContainer.className = "player";
+            break;
     }
-    lyricsOpen = !lyricsOpen;
 }
 
-function openTab() {
+function viewTransitionsWrapper(tabName = "") {
+    if (tabs.includes(tabOpen) && tabs.includes(tabName)) {
+        if (tabs.indexOf(tabName) > tabs.indexOf(tabOpen)) {
+            tabContentContainer.className = "tab-content scroll-right";
+        } else {
+            tabContentContainer.className = "tab-content scroll-left";
+        }
+    }
     if (document.startViewTransition) {
-        document.startViewTransition(toggleLyrics);
+        document.startViewTransition(() => switchTab(tabName));
     } else {
-        toggleLyrics();
+        switchTab(tabName);
     }
 }
 
-toggleButton.onclick = openTab;
-lyricsButton.onclick = openTab;
+function toggleTab(tabToToggle = "") {
+    if (tabOpen === tabToToggle) {
+        viewTransitionsWrapper("");
+    } else {
+        viewTransitionsWrapper(tabToToggle);
+    }
+}
+
+toggleButton.onclick = () => toggleTab(tabOpen || "lyrics");
+upNextButton.onclick = () => toggleTab("up-next");
+lyricsButton.onclick = () => toggleTab("lyrics");
+creditsButton.onclick = () => toggleTab("credits");
 document.onkeydown = (e) => {
     if (e.key === " ") {
         e.preventDefault();
-        openTab();
+        toggleTab(tabOpen || "lyrics");
     }
 };
